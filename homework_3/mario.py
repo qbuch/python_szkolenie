0ec6652c373cad60f1ab8872a84304a4dc8acd74
Jakub <kubaszw@gmail.com>
2021-01-24 22:56:36 +0100

    add mario.py

---
+++ homework_3/mario.py
@@ -0,0 +1,11 @@
+#Mario pyramid
+
+while True:
+    height = int(input("How high should the pyramid be?: "))
+    if height >= 1 and height <=8:
+        break
+
+for num in range(height):
+    print(" " * (height - num) + "#" * num)
+
+#nie wiem jak sie pozbyc pierwszej linii ktora wyswietal tylko spacje
