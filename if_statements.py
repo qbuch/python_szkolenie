482c096567ff42b3069a35b713efeba6a0985981
Jakub <kubaszw@gmail.com>
2021-01-25 19:07:12 +0100

    Merge branch 'main' of /home/kuba/python_infoshare/python_szkolenie with conflicts.

---
+++ if_statements.py
@@ -0,0 +1,18 @@
+# digit = 3
+#
+# if digit > 0:
+#     print(str(digit) + " is greater than 0")
+# elif digit < 0:
+#     print(str(digit) + " is less than 0")
+# else:
+#     print("Your input is 0")
+
+from pprint import pprint
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+pprint(matrix)
