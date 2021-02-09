bc003d82ee6b19849868f93c2783ac2c7cd12d73
Jakub <kubaszw@gmail.com>
2021-02-09 21:29:19 +0100

    final version

---
+++ homework_4/divisibility.py
@@ -0,0 +1,5 @@
+#wypisz wszystkie liczy z zakresu od 1 do 100, ktore sa podzielne przez 3 lub 7
+
+for i in range(1, 101):
+    if(i % 3==0) or (i %  7==0):
+        print(i)
---
+++ homework_4/dog_age.py
@@ -0,0 +1,18 @@
+# Napisz program obliczający wiek psa z ludzkich lat w psich latach.
+# Przez pierwsze dwa lata każdy psi rok to 10.5 ludzkiego, kolejne lata - psi rok to 4 ludzkie lata.
+# Na przykład:15 ludzkich lat to 73 psie lata. Dane wejściowe wprowadza użytkownik po uruchomieniuprogramu.
+
+
+human_age = int(input("Podaj wiek czlowieka: "))
+
+if human_age == 1:
+    print(10.5)
+elif human_age == 2:
+    print(21)
+elif human_age >= 3:
+    print("Twoj wiek w psich latach to " + str(((human_age - 2) * 4) + 21))
+
+
+###################
+
+
